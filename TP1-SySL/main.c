b85b9ee936f36f19d09f65ea0c6be21b2e244028
MarcosGurruchaga <83126979+MarcosGurruchaga@users.noreply.github.com>
2022-04-24 19:09:35 -0300

    Terminado

--- TP1-SySL/main.c
+++ TP1-SySL/main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    int c;
+    while(c != EOF) {
+    //Pedimos el caracter por consola
+    c = getchar();
+
+    //Si es uppercase lo hacemos lowercase
+    if (isupper(c))
+    c = tolower(c);
+
+    //Si es lowercase lo hacemos uppercase
+    else if (islower(c))
+    c=toupper(c);
+
+    //Chequeamos si no es digito para mostrarlo por consola
+    if (isdigit(c)==0)
+    putchar(c);
+
+
+
+    }
 }
